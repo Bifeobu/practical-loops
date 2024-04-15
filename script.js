60aacc94855e46b876071b833b47f9e66dda6205
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-14 20:09:59 -0400

    Fizz and Buzz

---
+++ script.js
@@ -0,0 +1,9 @@
+// Part 1: Fizz Buzz
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log(`${i}:Fizz`)
+    } 
+    if (i % 5 === 0) {
+        console.log(`${i}:Buzz`)
+    }
+}
