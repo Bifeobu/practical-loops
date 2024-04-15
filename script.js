3c475228d0c3887b0d7e8cd430d8cc3066153b4f
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-14 23:51:59 -0400

    finished part 2

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ for (let i = 1; i <= 100; i++) {
         console.log(i)
     }
 }
+
+console.log("========================")
+
+// Part 2: Prime Time
+
+// function isPrime(n) {
+//     if (n <= 1) return false;
+// for (let i = 2; i < n; i++) {
+//     if (n % i ===0) {
+//         return false;
+//     }
+//   }
+//   return true;
+// }
+// isPrime(4)
+
+function checkPrime(n) {
+    let i, flag = true;
+    for (i = 2; i <= n - 1; i++) {
+        if (n % i == 0) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag == true)
+        console.log(n + " is prime");
+    else
+        console.log(n + " is not prime");
+}
+checkPrime(4);
+checkPrime(5);
